25635e30c987b27fa2fe4bf9214bee0e07adc051
Siddfiridhi <sidd.finridhi@gmail.com>
2024-12-29 14:35:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+        host: '0.0.0.0', // Bind to 0.0.0.0 for Render to detect
+        port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use Render's port or default to 5173
+    },
+});
